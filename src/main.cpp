1fdc701951d458695092cc0cbca12c695b871127
Miggycelo <cmomarcelo@gmail.com>
2026-09-10 11:05:20 +0800

    Add rotary encoder volume logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,23 @@
 #include <Arduino.h>
 
-const int LED_PIN = 2;
-
-unsigned long previousMillis  = 0;
-const unsigned long interval = 2000;
-
-bool ledstate = false;
+const int DT_PIN = 26;
+int volume = 0;
 
 void setup(){
   Serial.begin(115200);
-
-  pinMode(LED_PIN, OUTPUT);
-
+  pinMode(DT_PIN, INPUT);
   Serial.println("ESP32 started");
-
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  int dstate = digitalRead(DT_PIN);
 
-  if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-
-      ledstate = !ledstate;
-      digitalWrite(LED_PIN, ledstate);
-
-      Serial.print("LED state: ");
-      Serial.println(ledstate);
+  if (dstate == HIGH){
+    volume++;
+  } else{
+    volume--;
   }
+  
+  Serial.print("Volume");
+  Serial.println(volume);
 }
